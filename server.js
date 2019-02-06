1932dc2a6d0fca6748a416cb588a6558d4e0774d
HyeUngPark <phu4860@gmail.com>
2019-02-06 21:44:09 +0900

    MongoDB Schema common draft

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 var port = process.env.PORT || 5000;
 var db = require('./mongo');
 var test = require('./testSchema');
+var schema = require('./schema/common/commonSchema');
 
 var server = app.listen(port,function(){
     console.log("★★★ Server Started ★★★");
@@ -28,7 +29,16 @@ app.get('/', function(req,res){
     test.create({
             connectIP:ipAddress
             ,connectTime:new Date()
-        });
+    });
+    schema.create({
+        workSection: 'PRODUCT' // 업무 구분 PRODUCT(상품) ORDER(주문) MEMBER(회원)
+       ,workDetailCode: 'PM' // 업무 상세코드 PM(상품관리) OM(주문관리) MM(회원관리)
+       ,firstWriteDate: new Date() // 최초작성일
+       ,subSchema:{ // 업무별 하위 스키마
+            test : 'test'
+       }
+    });
+    console.log(schema);
     res.redirect('./auth.html');
 });
 
