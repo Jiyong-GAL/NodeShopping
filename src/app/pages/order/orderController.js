233f64c12ebef5ce8903b6e8afa2c8f3240436d0
HyeUngPark <phu4860@gmail.com>
2019-02-23 22:08:00 +0900

    팝업 테스트

---
+++ src/app/pages/order/orderController.js
@@ -0,0 +1,32 @@
+/**
+ * @author a.demeshko
+ * created on 18.01.2016
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.ui.notifications')
+    .controller('orderController', orderController);
+
+  /** @ngInject */
+  function orderController($scope, $uibModal, baProgressModal) {
+    $scope.open = function (page, size) {
+      $uibModal.open({
+        animation: true,
+        templateUrl: page,
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+    };
+    $scope.openPop = function(){
+      window.open('about:blank','팝업','width=800, height=600');
+    }
+    $scope.openProgressDialog = baProgressModal.open;
+  }
+
+
+})();
