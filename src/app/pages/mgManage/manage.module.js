050a1e264b74f1738dd776657a1bf3f1071bd51f
heewon <hwchoi960131@naver.com>
2019-03-03 23:47:26 +0900

    관리자

--- src/app/pages/mgManage/manage.module.js
+++ src/app/pages/mgManage/manage.module.js
@@ -1,25 +1,26 @@
 /**
 * @author heewonchoi
 */
+
 (function () {
-  'use strict';
-  angular.module('BlurAdmin.pages.mgManage', [
-  //'BlurAdmin.pages.mgManage.memManage',
-  //'BlurAdmin.pages.mgManage.goodsManage',
-  ]).config(routeConfig);
+      'use strict';
+      
+      angular.module('BlurAdmin.pages.mgManage', [])
+          .config(routeConfig);
   
   /** @ngInject */
   function routeConfig($stateProvider) {
-  $stateProvider
-  .state('mgManage', {
-  url: '/mgManage',
-  templateUrl: 'app/pages/mgManage/incHeader.html',
-  title: '관리자페이지2',
-  sidebarMeta: {
-  icon: 'ion-gear-a',
-  order: 2000,
-  },
-  });
-  }
+      $stateProvider
+      .state('mgManage', {
+        url: '/mgManage',
+        templateUrl: 'app/pages/mgManage/incHeader.html',
+        title: '관리자페이지2',
+        controller: 'ModalsPageCtrl',
+        sidebarMeta: {
+          icon: 'ion-gear-a',
+          order: 2000,
+        },
+      });
+    }
   })();
   
